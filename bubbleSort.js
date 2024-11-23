3b3fe5f8825f492232242da3a812a0d1a073abf0
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-22 22:15:35 -0300

    build: if

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,8 +3,8 @@ let temp
 
 for (let i =0; i < array.length - 1; i++) {
     for (let j = 0; j < array.length - i - 1; j++) {
-        // if (array[j] > array[j + 1]) {
-        //     temp = arra
-        // }
+        if (array[j] > array[j + 1]) {
+            temp = arra
+        }
     }
 }

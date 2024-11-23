21e37735ea00e2cabb2b7b63f0d896c18257e244
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-22 22:23:38 -0300

    build:function

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,12 +1,16 @@
-const array = [5, 6, 1, 3]
-let temp 
+const array = [7, 5, 6, 1, 3]
+function bubbleSort (arr) {
+    let temp 
 
-for (let i =0; i < array.length - 1; i++) {
-    for (let j = 0; j < array.length - i - 1; j++) {
-        if (array[j] > array[j + 1]) {
-            temp = array[j]
-            array[j] = array[j + 1]
-            array[j + 1] = temp
+    for (let i =0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
         }
     }
+    return array
 }
+console.log(bubbleSort(array))

1ab3043238f20854d2bf2c46fe1462c22bd41826
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-23 16:24:34 -0300

    atualizando arrays

--- findMaximum.js
+++ findMaximum.js
@@ -1,3 +1,3 @@
-const array1 = [1, 2, 3, 4, 5]
-const array2 = [5, 4, 3, 2, 1]
-const array3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const array1 = [4, 2, 3]
+const array2 = [1, 1, 2, 3]
+const array3 = [1, 4]

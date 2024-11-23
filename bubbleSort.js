e959c2e3b513544665a048c9b9276e33523d0cd5
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-22 22:15:11 -0300

    build: two loops for

---
+++ bubbleSort.js
@@ -0,0 +1,10 @@
+const array = [5, 6, 1, 3]
+let temp 
+
+for (let i =0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+        // if (array[j] > array[j + 1]) {
+        //     temp = arra
+        // }
+    }
+}

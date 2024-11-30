9897c858c320711261ca7de605aeac4b0fe792c4
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-30 15:34:18 -0300

    testando funcao binarySearch

--- jest.test.js
+++ jest.test.js
@@ -1,5 +1,9 @@
-const findMaxVal = require('./maximizeArray')
+const {findMaxVal, binarySearch }= require('./maximizeArray')
 
 test('findMaxVal', () => {
     expect(findMaxVal([2, 1, 7], 3, 3, 7)).toBe(7)
+})
+
+test('binarySearch', () => {
+    expect(binarySearch([1, 2, 3, 4, 5], 1)).toBe(0)
 })

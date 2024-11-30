41118c62979a4b1a46049333b21144eb812107de
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-29 21:12:29 -0300

    encontrando valor na ultima linha

--- maximizeArray.js
+++ maximizeArray.js
@@ -15,6 +15,17 @@ function maximizeArray (arr, number, maxValue) {
         dp[0][number - arr[0]] = 1
     }
 
-    
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j <= maxValue; j++) {
+            if (dp[i - 1][j] > 0) {
+                if (0 <= j + arr[i] && j + arr[i] <= maxValue) {
+                    dp[i][j + arr[i]] = Math.max(dp[i][j + arr[i]], dp[i - 1][j] + 1)
+                }
+                if (0 <= j - arr[i] && j - arr[i] <= maxValue) {
+                    dp[i][j - arr[i]] = Math.max(dp[i][j - arr[i]], dp[i - 1][j] + 1)
+                }
+            }
+        }
+    }
 
 }

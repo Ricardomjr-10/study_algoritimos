38ef602a36a91f48ef5963580ad3b11327e3a09e
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-29 21:38:02 -0300

    fix

--- maximizeArray.js
+++ maximizeArray.js
@@ -1,6 +1,3 @@
-const array = [2, 1, 7]
-const number = 3
-const maxValue = 7
 
 let ans = 0
 
@@ -15,17 +12,16 @@ function maximizeArray(arr, n, num, maxLimit, ind) {
     }
 
     if (num + arr[ind] <= maxLimit) {
-        maximizeArray(arr, n. num + arr[ind], maxLimit, ind + 1)
+        maximizeArray(arr, n.num + arr[ind], maxLimit, ind + 1)
     }
 }
 
-function findMaxVal (arr, n, num, maxLimit) {
+function findMaxVal(arr, n, num, maxLimit) {
     let ind = 0
     maximizeArray(arr, n, num, maxLimit, ind)
     return ans
 }
 
 
-console.log(maximizeArray(array, array.length, number, maxValue))
 
 

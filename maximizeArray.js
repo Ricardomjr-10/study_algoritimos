ee4b8cb38f8ba56ab0e10fef445d16b8d66c04b8
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-29 21:02:40 -0300

    construindo function

--- maximizeArray.js
+++ maximizeArray.js
@@ -2,3 +2,10 @@ const array = [2, 1, 7]
 const number = 3
 const maxValue = 7
 
+function maximizeArray (arr, number, maxValue) {
+    const n = arr.length
+    const dp = new Array(n).fill(0).map(() => {
+        return new Array(maxValue + 1).fill(0)
+    })
+
+}

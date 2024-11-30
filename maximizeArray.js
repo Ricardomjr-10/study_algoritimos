8814d1b510d3259c4d1208dca798cb0ef0b7773c
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-29 21:42:13 -0300

    fix correct

--- maximizeArray.js
+++ maximizeArray.js
@@ -1,27 +1,46 @@
+let ans = 0;
+function findMaxValUtil(arr, n, num, maxLimit, ind)
+{
+	
+	
+	if (ind == n) 
+	{
+		ans = Math.max(ans, num);
+		return;
+	}
 
-let ans = 0
+	
+	if (num - arr[ind] >= 0)
+	{
+		findMaxValUtil(arr, n, num - arr[ind], 
+					maxLimit, ind + 1);
+	}
 
-function maximizeArray(arr, n, num, maxLimit, ind) {
-    if (ind == n) {
-        ans = Math.max(ans, num)
-        return
-    }
-
-    if (num - arr[ind] >= 0) {
-        maximizeArray(arr, n, num - arr[ind], maxLimit, ind + 1)
-    }
-
-    if (num + arr[ind] <= maxLimit) {
-        maximizeArray(arr, n.num + arr[ind], maxLimit, ind + 1)
-    }
+	if (num + arr[ind] <= maxLimit)
+	{
+		findMaxValUtil(arr, n, num + arr[ind], 
+					maxLimit, ind + 1);
+	}
 }
 
-function findMaxVal(arr, n, num, maxLimit) {
-    let ind = 0
-    maximizeArray(arr, n, num, maxLimit, ind)
-    return ans
+
+function findMaxVal(arr, n, num, maxLimit)
+{
+	
+
+	let ind = 0;
+
+	findMaxValUtil(arr, n, num, maxLimit, ind);
+
+	return ans;
 }
 
 
+let num = 3;
+let arr = [ 2, 1, 7];
+let n = arr.length;
+let maxLimit = 7;
 
 
+console.log(findMaxVal(arr, n, num, maxLimit));
+							
